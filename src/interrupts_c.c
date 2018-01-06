101bd9607ec413b4fe0fcb359d6f8f65f3cf8771
Will Ransohoff <wransohoff@gmail.com>
2018-01-06 09:39:00 -0800

    Updated interrupt to check its EXTI line

--- src/interrupts_c.c
+++ src/interrupts_c.c
@@ -4,9 +4,10 @@
  * A button on pin A4 will use the [4:15] interrupt.
  */
 void EXTI4_15_IRQ_handler(void) {
-    interrupt_state = !interrupt_state;
-    // Reset the 'pending' flag to acknowledge the interrupt.
-    // For now only one EXTI interrupt is active, but it is best to
-    // check which event you're responding to.
-    EXTI->PR |= EXTI_PR_PR4;
+    if (EXTI_GetITStatus(EXTI_Line4) != RESET) {
+        interrupt_state = !interrupt_state;
+        // Reset the 'pending' flag to acknowledge the interrupt.
+        // Or w/periph library: EXTI_ClearITPendingBit(EXTI_Line4);
+        EXTI->PR |= EXTI_PR_PR4;
+    }
 }
